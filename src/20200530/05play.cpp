eeed43e5a0e44328f94661c62966a7ef2fee85ef
liupeng <liupeng.0@outlook.com>
2020-05-30 00:58:16 +0800

    进制转换 V 0.0.5

---
+++ src/20200530/05play.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 刘鹏 on 2020/5/30.
+//
+
+#include<iostream>
+#include<string>
+
+void paly05() {
+
+    int a;
+    while(std::cin >> std::hex >> a) {
+        std::cout <<  a;
+    }
+}
