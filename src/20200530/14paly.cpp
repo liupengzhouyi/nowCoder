fcb141dea484977279b9ea5114e9929f0433aa43
liupeng <liupeng.0@outlook.com>
2020-05-30 23:03:30 +0800

    句子逆序 V 0.0.14

--- src/20200530/14paly.cpp
+++ src/20200530/14paly.cpp
@@ -1,14 +1,27 @@
+//
+// Created by 刘鹏 on 2020/5/30.
+//
+
 #include <iostream>
 #include <vector>
 #include <map>
 #include <string>
 #include <sstream>
 
-int main()
+int paly15()
 {
     std::string newStr;
     getline(std::cin, newStr);
-    std::string temp;
+    std::stringstream ss(newStr);
+    std::string res="", tmp;
+    while (ss>>tmp) {
+        if (res == "")
+            res = tmp;
+        else
+            res = tmp + " " + res;
+    }
+    std::cout << res;
+    /*std::string temp;
     std::string out = "";
     for (int i = newStr.length(); i >= 0; i--) {
         if (newStr[i] != ' ') {
@@ -19,7 +32,7 @@ int main()
         }
     }
     out = out + temp;
-    std::cout << out << std::endl;
+    std::cout << out.length() << std::endl;*/
     return 0;
 }
 
