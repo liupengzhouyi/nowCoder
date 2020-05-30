0bd93cd474b035a1f657818e838d19e58a6fd5a8
liupeng <liupeng.0@outlook.com>
2020-05-31 01:03:43 +0800

    简单错误记录 V 0.0.17

--- main.cpp
+++ main.cpp
@@ -1,29 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
 
-//求int型正整数在内存中存储时1的个数
-int main()
-{
-    int count = 1;
-    int time;
-    std::cin >> time;
-    std::string sum = "";
-    while (time != 1) {
-        if (time % 2 == 1) {
-            sum = '1' + sum;
-            count++;
-            time = (time - 1) / 2;
-        } else {
-            sum = '0' + sum;
-            time = time / 2;
-        }
-    }
-    sum = '1' + sum;
-    std::cout << count << std::endl;
-    return 0;
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string getFileName(string path){
+    int pos = path.rfind('\\');
+    return path.substr(pos + 1);
 }
 
+string modifyName(string name){
+    if (name.size() > 16){
+        name = name.substr(name.size() - 16);
+    }
+
+    return name;
+}
+
+struct ErrRecord{
+    string file;
+    int lineNo;
+    int count;
+
+    ErrRecord(string file, int lineNo){
+        this->file = file;
+        this->lineNo = lineNo;
+        count = 1;
+    }
+
+    bool operator==(const ErrRecord & a){
+        return (file == a.file) && (lineNo == a.lineNo);
+    }
+};
+
+int main(){
+
+    string file;
+    int lineNo;
+    vector<ErrRecord> myvec;
+    while (cin >> file >> lineNo){
+        ErrRecord record(getFileName(file), lineNo);
+        auto res = find(myvec.begin(), myvec.end(), record);
+        if (res == myvec.end()){
+            myvec.push_back(record);
+        }
+        else{
+            res->count++;
+        }
+    }
+
+    int count = 0;
+    for (auto item : myvec){
+        if (count + 8 >= myvec.size()){
+            cout << modifyName(item.file) << " " << item.lineNo << " "
+                 << item.count << endl;
+        }
+        count++;
+    }
 
 
+    return 0;
+}
---
+++ src/20200531/17paly.cpp
@@ -0,0 +1,173 @@
+//
+// Created by 刘鹏 on 5/31/2020.
+//
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <list>
+
+
+class item {
+    int inedx;
+    std::string dir;
+    std::string name;
+    std::string number;
+
+public:
+    item() {}
+
+    int getInedx() const {
+        return inedx;
+    }
+
+    void setInedx(int inedx) {
+        item::inedx = inedx;
+    }
+
+    const std::string &getDir() const {
+        return dir;
+    }
+
+    void setDir(const std::string &dir) {
+        item::dir = dir;
+    }
+
+    const std::string &getName() const {
+        return name;
+    }
+
+    void setName(const std::string &name) {
+        item::name = name;
+    }
+
+    const std::string &getNumber() const {
+        return number;
+    }
+
+    void setNumber(const std::string &number) {
+        item::number = number;
+    }
+};
+
+int paly17()
+{
+    std::string a, b;
+    std::list <item> myList;
+    int index = 0;
+    while(std::cin >> a) {
+        std::cin >> b;
+        /*if (b == "0001") {
+            break;
+        }*/
+        if (index > 8) {
+            myList.pop_back();
+            index = index -1;
+        }
+        std::string name = "";
+        for (int i = a.length(); i > 0; i--) {
+            if (a[i] == '\\') {
+                break;
+            } else {
+                name = a[i] + name;
+            }
+        }
+        std::string newName = "";
+        if (name.length() > 16) {
+            for (int i = name.length() - 16 - 1; i < name.length(); i++) {
+                newName = newName + name[i];
+            }
+        } else {
+            newName = name;
+        }
+        item temp = item();
+        temp.setDir(a);
+        temp.setNumber(b);
+        temp.setName(newName);
+        temp.setInedx(1);
+        // std::cout << temp.getName() << " " << temp.getNumber() << " " << temp.getInedx() << std::endl;
+        std::list<item>::iterator iter1;
+        bool key = false;
+        for(iter1 = myList.begin(); iter1 != myList.end() ;iter1++)
+        {
+            if (iter1->getName() == temp.getName()) {
+                if (iter1->getNumber() == temp.getNumber()) {
+                    iter1->setInedx(iter1->getInedx() + 1);
+                    key = true;
+                }
+            }
+        }
+        if (key == false) {
+            myList.push_front(temp);
+            index = index + 1;
+        }
+        // std::list<item>::iterator iter1;
+        /*for(iter1 = myList.begin(); iter1 != myList.end() ;iter1++)
+        {
+            std::cout << iter1->getName() << " " << iter1->getNumber() << " " << iter1->getInedx() << std::endl;
+        }*/
+    }
+    myList.pop_back();
+    std::list<item>::reverse_iterator iter1;
+    for(iter1 = myList.rbegin(); iter1 != myList.rend() ;iter1++)
+    {
+        std::cout << iter1->getName() << " " << iter1->getNumber() << " " << iter1->getInedx() << std::endl;
+    }
+    return 0;
+}
+
+
+
+/*
+
+C:\gtu\vcy\jk\zwthkipl 636
+G:\rsle\lsax\yalcxu\vwhysms 637
+F:\fzqz 640
+E:\lswb\styce\thjnbxdvg 645
+F:\up\qflvvayylipvj 635
+C:\pivw\rkd 644
+E:\tlkbjb\pcvnvm\qh\fzqz 633
+E:\ezke\xvrdkuesnjerakzhs 641
+E:\qflvvayylipvj 639
+F:\ybpxk 644
+G:\rfdr\faxpyhyznsssbo 630
+E:\xktax\osekgaqy 646
+C:\te 633
+G:\albed\ffc\jclzfq\h 631
+F:\zsshil\pkue\mazocurlwufmkkrw 635
+E:\dmj\uefo\syabgm\qflvvayylipvj 643
+F:\pgdiwvceluyzft 634
+D:\xiqck\dta\lnh 639
+E:\nu\nz\wwjpch\fzqz 650
+E:\gfsahypjzwfglvou 644
+G:\qh\thjnbxdvg 641
+C:\cp\voause\mf\hebgpyzpyyybiywpv 632
+E:\de\vet\vdooytekbghohqz 631
+E:\cox\pvg\tlrlr 640
+G:\hkc\fllkwmwlgiqahxbfs 638
+E:\apjnogffvkwnv 637
+F:\wgotle\ar\wgh\hebgpyzpyyybiywpv 639
+F:\wgotle\ar\wgh\hebgpyzpyyybiywpv 639
+G:\nrekjavm 634
+C:\mvngfg\lfjnvz\xvrdkuesnjerakzhs 634
+F:\exne\vh\kqh\fzqz 641
+F:\lepvz 642
+G:\qg\hcbh\uknyte\pgdiwvceluyzft 636
+F:\ho\cn\uyobrilaabapini 637
+E:\xbc\fzqz 642
+G:\ju\nmdd\fzqz 643
+G:\lzpmdx\rxp\ybpxk 629
+D:\fc\lnh 629
+D:\dsgvo\zwthkipl 631
+G:\pujc\cgorfjzkqmnjathbiip 640
+G:\vfhyp\cs\rrt 638
+F:\oteh\ti\urajfw\tlrlr 636
+C:\crdp\qwptjlorrmnv 642
+D:\kcl\fqzs\zlm\pgdiwvceluyzft 640
+G:\faxpyhyznsssbo 639
+D:\swnhezhgdcwwbmkyqt 637
+D:\azo\sry\faxpyhyznsssbo 633
+C:\beqt\eh\dm\dlhehjccfdgrrzyj 642
+F:\lepvz 635
+ */
