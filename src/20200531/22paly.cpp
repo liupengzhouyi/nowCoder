aae626aba622e3366cce3b02cb807131cb0f6a44
liupeng <liupeng.0@outlook.com>
2020-05-31 22:59:37 +0800

    汽水瓶 V 0.0.22

---
+++ src/20200531/22paly.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 刘鹏 on 2020/5/31.
+//
+#include <iostream>
+using namespace std;
+int paly22(){
+    int n;
+    while(cin>>n){
+        if(n!=0)
+            cout<<n/2<<endl;
+    }
+    return 0;
+}
