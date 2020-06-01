5d29d13765327faa21aad4a03fd30d6976754e51
liupeng <liupeng.0@outlook.com>
2020-06-01 23:44:37 +0800

    尼科彻斯定理 V 0.0.58

---
+++ src/20200601/58paly.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 刘鹏 on 2020/6/1.
+//
+
+#include <iostream>
+using namespace std;
+// 解方程即可。b代表第一个奇数，等差数列求和
+
+string GetSequeOddNum(int m)
+{
+    int b =  m*(m-1) + 1;  // 一定是奇数
+    cout << b;
+    for(int i = 1; i < m; i++)
+    {
+        cout <<"+"<<b+i*2;
+    }
+    cout<<endl;
+    return "";
+}
+
+
+
+int paly58()
+{
+    int m;
+    while(cin >> m)
+    {
+        GetSequeOddNum(m);
+    }
+}
