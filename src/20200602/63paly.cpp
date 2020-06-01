6334e51d658ec8bab740d7ea9883cbf3b0a3d54b
liupeng <liupeng.0@outlook.com>
2020-06-02 00:03:19 +0800

    等差数列 V 0.0.63

---
+++ src/20200602/63paly.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 刘鹏 on 2020/6/2.
+//
+
+#include<iostream>
+using namespace std;
+
+int paly63()
+{
+    int n;
+    while(cin>>n)
+        cout<<(3*n+1)*n/2<<endl;
+    return 0;
+}
