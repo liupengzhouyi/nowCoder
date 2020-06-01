86cfd54bba519aace40c8bed2949e9b31b291e30
liupeng <liupeng.0@outlook.com>
2020-06-01 08:31:45 +0800

    查找组成一个偶数最接近的两个素数 V 0.0.46

---
+++ src/20200601/46paly.cpp
@@ -0,0 +1,36 @@
+//
+// Created by 刘鹏 on 2020/6/1.
+//
+
+#include<iostream>
+using namespace std;
+
+bool prime_num(int n)
+{
+    bool flag=true;
+    for(int i=2;i<n-1;i++)
+    {
+        if(n%i==0)
+        {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
+int paly45()
+{
+    int n;
+    while(cin>>n)
+    {
+        for(int i=n/2;i>=2;i--)
+            if(prime_num(i) && prime_num(n-i))
+            {
+                cout<<i<<endl;
+                cout<<n-i<<endl;
+                break;
+            }
+    }
+    return 0;
+}
